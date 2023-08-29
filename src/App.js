f4e434cdcbab242016ad5e70f11dc8e5e054722b
silver1124 <yashtiwari1124>
2023-08-29 17:35:57 +0530

    Protecting the product page

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import Store from './NavBar.js/Store';
 import AboutNav from './NavBar.js/AboutNav';
 import HomeNav from './NavBar.js/HomeNav';
 import ContactNav from './NavBar.js/ContactNav';
 import ProductDetails from './pages/ProductDetails';
-import Login from './pages/Login';
+import LoginNav from './NavBar.js/LoginNav';
+import React,{useContext} from 'react'
+import AuthContext from './store/auth-context';
 
 function App() {
+    const authCtx= useContext(AuthContext);
+    const loggedIn = authCtx.isLoggedIn;
+
   return (
     <Router>
       <Routes>
         <Route path="/Home" element={<HomeNav />} />
-        <Route path="/" element={<Store />} />
+        { loggedIn && <Route path="/" element={<Store />} />}
         <Route path="/About" element={<AboutNav />} />
-        <Route path='/Login' element={<Login/>}/>
+        { !loggedIn && <Route path='/Login' element={<LoginNav/>}/>}
         <Route path="/Contact" element={<ContactNav />} />
         <Route path='/products/:id' element={<ProductDetails/>}/>
       </Routes>
